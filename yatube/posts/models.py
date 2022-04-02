6e620d8f6de1d2fdb48ff81d6ffa50705db3842c
Руслан Николаев <rusnikolaev1@gmail.com>
2022-04-02 10:08:59 +0500

    Finished 3 sprint commit

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+
+class Post(models.Model):
+    #Содержание поста
+    text = models.TextField()
+    #Дата публи кации
+    pub_date = models.DateTimeField( auto_now_add=True)
+    #Автор
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name = 'posts'
+    )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.CASCADE,
+        related_name = 'posts', 
+        blank = True, 
+        null = True
+    )
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    #уникальный адресс группы, часть URL
+    slug = models.SlugField(unique=True)
+    #тескт, описывающий сообщество
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
+
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+#импортируем загрузчик
+from django.template import loader
+from .models import Post, Group
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title' : title, 
+        'posts' : posts
+    }
+    return render(request, template, context) 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Групповые публикации {slug}')
+    template = 'posts/group.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group':group,
+        'posts':posts
+    }
+    return render(request, template, context) 
